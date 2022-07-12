cc4cfe535223387708d18149522c2ad6247b6dfc
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-12 00:04:38 -0700

    configured cors, deployment testing

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.use(cors());
 
 app.use(cors({
     origin: (origin, callback) => {
-        // if (origin !== CLIENT_URL && origin !== SERVER_URL) {
-        //     return callback("Access to this host is denied", false);
-        // } else {
-        return callback(null, true);
-        // }
+        if (origin !== CLIENT_URL && NODE_ENV === "production") {
+            return callback("Access to this host is denied", false);
+        } else {
+            return callback(null, true);
+        }
     }
 }));
 app.use("/room", room);
